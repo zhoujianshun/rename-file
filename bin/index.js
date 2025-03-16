c39badda563a8640866118ca38e28807b254092a
jshzhou <jshzhou@126.com>
2025-03-16 15:24:28 +0800

    重构目录

--- bin/index.js
+++ bin/index.js
@@ -1,32 +1,5 @@
 #!/usr/bin/env node
 
-const { replaceFileName } = require('./actions')
-const { Command } = require('commander')
-const program = new Command()
+const program = require('../src/index')
 
-program.name('rename-file').description('CLI to rename file').version('0.1.0')
-
-// 替换文件名
-program
-  .command('replace')
-  .description('replace file name')
-  .argument('<srcDir>', '重命名文件夹中的文件')
-  .argument('<searchValue>', '要替换的字符串')
-  .argument('[replaceString]', '新的字符串，不填则删除')
-  .option('-r, --rename-meta-title', '是否同步修改视频metaData中的title为文件名')
-  .option('-u, --use-folder-name', '是否使用文件夹作为文件名')
-  .action((srcDir, searchValue, replaceString = '', options) => {
-    replaceFileName(srcDir, searchValue, replaceString, options)
-  })
-
-// // 使用文件夹作为文件名
-// program
-//   .command('usefolder')
-//   .description('use folder as file name')
-//   .argument('<srcDir>', '重命名文件夹中的文件')
-//   .option('--rmt', '是否同步修改视频metaData中的title为文件名')
-//   .action((srcDir, options) => {
-//     replaceFileName(srcDir, searchValue, replaceString, options)
-//   })
-
-program.parse()
+module.exports = program
